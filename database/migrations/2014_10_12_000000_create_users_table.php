b9ff0e1f6d6b6e68dc4263f871fd43f1b01f91ed
Nitoich <bcmamka@gmail.com>
2022-02-01 17:49:24 +0300

    Сделал миграции и создал модели

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,11 +15,14 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->string('full_name');
+            $table->string('last_name');
+            $table->string('first_name');
+            $table->string('login');
             $table->string('password');
-            $table->rememberToken();
+            $table->string('salt', 5);
+            $table->string('api_token')->nullable();
+            $table->enum('role', ['guest', 'teacher', 'operator', 'admin', 'student'])->default('guest');
             $table->timestamps();
         });
     }
